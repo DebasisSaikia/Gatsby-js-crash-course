502cbf7a79a59c97aee2620be93cdcdc210901ae
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-22 00:21:04 +0530

    static images

--- recipe-project/src/demo/Images.js
+++ recipe-project/src/demo/Images.js
@@ -8,11 +8,17 @@ const Wrapper = styled.section`
   } */
   width: 70vw;
   margin: 0 auto;
+  display: grid;
+  text-align: center;
+  gap: 2rem;
   article {
     border: 2px solid red;
   }
   .ex-img {
     border-radius: 1rem;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr 1fr;
   }
 `
 
@@ -20,16 +26,38 @@ const Images = () => {
   return (
     <Wrapper>
       <article>
+        <h2>Constrained </h2>
         <StaticImage
           src="../assets/images/recipe-1.jpeg"
           alt="gatsby-image"
-          height={400}
           placeholder="tracedSVG"
           layout="constrained"
           className="ex-img"
           as="section"
         />
-        <h2>Gatsby images</h2>
+      </article>
+      <article>
+        <h2>Fixed </h2>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="gatsby-image"
+          placeholder="blurred"
+          layout="fixed"
+          width={200}
+          className="ex-img"
+          as="div"
+        />
+      </article>
+      <article>
+        <h2>FullWidth </h2>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="gatsby-image"
+          placeholder="dominantColor"
+          layout="fullWidth"
+          className="ex-img"
+          as="article"
+        />
       </article>
     </Wrapper>
   )
