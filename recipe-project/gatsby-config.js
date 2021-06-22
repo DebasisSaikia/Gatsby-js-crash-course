d7f6d5c34933678bdd96c74188abc41f4ded56e1
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-22 13:54:42 +0530

    first graphql query

--- recipe-project/gatsby-config.js
+++ recipe-project/gatsby-config.js
@@ -1,4 +1,15 @@
 module.exports = {
+  siteMetadata: {
+    title: "Awesome Recipes",
+    description: "Yummy and clean recipe site",
+    author: "Debasis",
+    person: { name: "deba", age: 24 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "deba", age: 24 },
+      { name: "anant", age: 25 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
