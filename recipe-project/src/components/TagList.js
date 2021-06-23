a05ef6d970fc7ba4edc99455e78934f7cad7ef34
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-23 16:37:09 +0530

    tags page completed

--- recipe-project/src/components/TagList.js
+++ recipe-project/src/components/TagList.js
@@ -1,12 +1,22 @@
+import { Link } from "gatsby"
 import React from "react"
 import setupTags from "../utils/setupTags"
 
 const TagList = ({ recipes }) => {
   const newTags = setupTags(recipes)
-  console.log(newTags)
   return (
-    <div>
-      <h2>Tag lists</h2>
+    <div className="tag-container">
+      <h4>Recipes</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          const [text, value] = tag
+          return (
+            <Link to={`/${text}`} key={index}>
+              {text} ({value})
+            </Link>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- recipe-project/src/pages/tags.js
+++ recipe-project/src/pages/tags.js
@@ -1,12 +1,40 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import setupTags from "../utils/setupTags"
 
-const tags = () => {
+const tags = ({ data }) => {
+  const newTags = setupTags(data.allContentfulRecipe.nodes)
+  console.log(newTags)
   return (
     <Layout>
-      <h1>Tags</h1>
+      <main className="page">
+        <section className="tags-page">
+          {newTags.map((tag, index) => {
+            const [text, value] = tag
+            return (
+              <Link to={`/${text}`} key={index} className="tag">
+                <h5>{text}</h5>
+                <p>{value} recipe</p>
+              </Link>
+            )
+          })}
+        </section>
+      </main>
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allContentfulRecipe {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`
 
 export default tags
