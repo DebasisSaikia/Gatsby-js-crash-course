31c6315eef4ed7432eb232763e0f6de0fcd85ed5
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-26 15:48:15 +0530

    layout file added

---
+++ blog-site/src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+
+export default function Home() {
+  return (
+    <Layout>
+    <section>
+      <div>
+        <h2>design</h2>
+        <h3>Develop & Deploy</h3>
+      </div>
+    </section>
+    </Layout>
+  )
+}
---
+++ blog-site/src/pages/projects/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+
+const Projects = () => {
+    return (
+        <Layout>
+            <h1>My Projects</h1>
+        </Layout>
+    )
+}
+
+export default Projects
